315f60502157181580f604733c0f31e80f696e1c
shamilakhan936 <shamilak936@gmail.com>
2024-03-09 14:27:44 +0500

    setup clerk sign-in and sign-up

---
+++ app/(root)/layout.js
@@ -0,0 +1,18 @@
+import { ClerkProvider } from '@clerk/nextjs';
+
+import '../globals.css';
+
+export const metadata = {
+    title: 'Next js',
+    description: 'Permian-test-task'
+}
+
+export default function RootLayout({children}) {
+    return (
+        <ClerkProvider>
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+        </ClerkProvider>
+    )
+}
